80d618ae7484a8c43098b7fcc7d87d7b94e4d562
Sri Harsha <sri_harsha509@hotmail.com>
2020-01-30 20:41:32 +0530

    Add: [Java] adding drag and drop by code sample

---
+++ java/mouse_actions/dragAndDropBy.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class dragAndDropBy {
+  public static void main(String[] args) {
+    WebDriver driver = new ChromeDriver();
+    try {
+      // Navigate to Url
+      driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+
+      // Store 'box A' as source element
+      WebElement sourceEle = driver.findElement(By.id("column-a"));
+
+      // Store 'box B' as source element
+      WebElement targetEle = driver.findElement(By.id("column-b"));
+      int targetEleXOffset = targetEle.getLocation().getX();
+      int targetEleYOffset = targetEle.getLocation().getY();
+
+      Actions actionProvider = new Actions(driver);
+      // Performs dragAndDropBy onto the  target element offset position
+      actionProvider.dragAndDropBy(sourceEle, targetEleXOffset, targetEleYOffset).build().perform();
+    } finally {
+      driver.quit();
+    }
+  }
+}
